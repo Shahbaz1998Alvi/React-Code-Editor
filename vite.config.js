837f9ee693c7136906cd949126189e1155382487
Shahbaz Alvi <164075491+Shahbaz1998Alvi@users.noreply.github.com>
2024-06-20 18:12:26 +0530

    commit changes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 
 export default defineConfig({
+  base:"/ResultSummary-Component/",
   plugins: [reactRefresh()],
   base: "/React-Code-Editor/"
 });
